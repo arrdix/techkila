a02cb932ebbd910f957d10beea1708565c756a60
arrdix <mail.ardix@gmail.com>
2024-09-04 18:21:45 +0700

    style(app): improve accent gradient

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -10,14 +10,14 @@ export function RootLayout() {
             <Nav />
             <Outlet />
             <Footer />
-            <div className="flex justify-center items-center w-16 h-16 bg-foreground bg-opacity-5 rounded-full fixed bottom-[100px] right-[25px]">
+            <div className="flex justify-center items-center w-16 h-16 bg-foreground bg-opacity-5 rounded-full fixed bottom-[100px] right-[25px] shadow-lg">
                 <TbChevronsUp size={36} />
             </div>
-            <div className="flex justify-center items-center w-16 h-16 bg-foreground bg-opacity-5 rounded-full fixed bottom-[25px] right-[25px]">
+            <div className="flex justify-center items-center w-16 h-16 bg-foreground bg-opacity-5 rounded-full fixed bottom-[25px] right-[25px] shadow-lg">
                 <FaWhatsapp size={36} />
             </div>
             <div className="w-[1000px] h-[1000px] bg-gradient-to-r from-cyan-300 from-40% via-cyan-100 via-60% to-pink-400 to-90% absolute top-[250px] right-0 translate-x-56 rounded-full blur-[185px] -z-40" />
-            <div className="w-[1000px] h-[1500px] bg-gradient-to-r from-cyan-300 from-40% via-cyan-400 via-60% to-cyan-300 to-90% absolute bottom-[1250px] lg:bottom-[850px] left-0 -translate-x-[400px] rounded-full blur-[150px] -z-40" />
+            <div className="w-[1000px] h-[1500px] bg-gradient-to-r from-cyan-300 from-40% via-cyan-400 via-60% to-cyan-300 to-90% absolute bottom-[1250px] lg:bottom-[850px] left-0 -translate-x-[400px] rounded-full blur-[125px] -z-40" />
         </div>
     )
 }
